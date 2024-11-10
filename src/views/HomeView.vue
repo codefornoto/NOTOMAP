70060a590f5978e9497b302093ead4a86a006590
kenshgy <ken.shgy.1386@gmail.com>
2024-11-10 20:59:35 +0900

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import 'leaflet/dist/leaflet.css'
+import { LMap, LTileLayer } from '@vue-leaflet/vue-leaflet'
+import L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+import { ref } from 'vue'
+
+delete L.Icon.Default.prototype._getIconUrl
+L.Icon.Default.mergeOptions({})
+
+const zoom = ref(2)
+</script>
+
+<template>
+  <div style="height: 600px; width: 800px">
+    <l-map ref="map" v-model:zoom="zoom" :center="[47.41322, -1.219482]">
+      <l-tile-layer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        layer-type="base"
+        name="OpenStreetMap"
+      ></l-tile-layer>
+    </l-map>
+  </div>
+</template>
+
+<style></style>
