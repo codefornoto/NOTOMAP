ef4104995a586db207be50c76630ae4721680efb
kenshgy <ken.shgy.1386@gmail.com>
2024-11-11 12:24:47 +0900

    outDir設定

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/NOTOMAP',
+  build: {
+    outDir: 'docs',
+  },
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
